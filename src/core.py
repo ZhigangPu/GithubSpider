c58f2278d5aa9d29708af3d773158d281067ab9b
Ayron.Pu <zhigang.pu@yitu-inc.com>
2019-06-01 15:29:42 +0800

    [INIT]

--- src/core.py
+++ src/core.py
@@ -1,2 +1,12 @@
 #! /usr/bin/python3
 # author: ayron.pu
+
+import requests
+
+url = 'https://github.com/explore?since=daily#trending'
+
+headers = {}
+headers['Content-type'] = 'application/json'
+
+r = requests.get(url=url, headers=headers)
+
